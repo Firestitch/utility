611bd7914c15d9961c12fb1bebc8ac35a445a9cd
Raymond Gigliotti <ray@firestitch.com>
2018-09-04 15:40:39 -0400

    switched to web bootstrap

--- web/index.php
+++ web/index.php
@@ -1,12 +1,10 @@
 <?php
-	$has_bootstrap = @include_once(realpath(dirname(__FILE__)."/../..")."/framework/boot/bootstrap.inc");
+	$has_bootstrap = @include_once(realpath(dirname(__FILE__)."/../..")."/framework/boot/webbootstrap.inc");
 	
 	if(!$has_bootstrap) {
 		header("location: /unavailable/");
 		die;
 	}
-		
-	BASE_APPLICATION::initialize_web_application();	
 
 	$application = APPLICATION::get_instance();
 	$application->initialize();	
