e58ef4fc631ddef778041b5d44dabf38f663fac9
Ray <ray@email.com>
2021-08-04 11:28:22 -0400

    Boot/command.ph

--- web/index.php
+++ web/index.php
@@ -2,7 +2,7 @@
 
 use Framework\Core\WebApplication;
 
-$hasBootstrap = @(include_once dirname(__FILE__) . "/../../framework/Boot/webbootstrap.inc");
+$hasBootstrap = @(include_once dirname(__FILE__) . "/../../framework/Boot/web.php");
 
 if (!$hasBootstrap) {
   header("location: /unavailable/");
